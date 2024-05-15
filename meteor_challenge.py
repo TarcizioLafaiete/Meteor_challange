0528b91b6a150a2953ec2d808096a6e2099163bd
Tarcizio Lafaiete <tarcizio.lafaiete@inventvision.com.br>
2024-05-15 17:52:50 -0300

    refactor: melhorando a solução reduzindo o número de acessos a matriz

--- meteor_challenge.py
+++ meteor_challenge.py
@@ -10,80 +10,61 @@ class Image_Objects(Enum):
     GROUND =  [0,0,0]
 
 
+def map_objects_coord(image:np.ndarray) -> tuple[list,list,list]:
+    lake_coords = []
+    star_coords = []
+    meteor_coords = []
 
-def counting_pixel(image:np.ndarray,pattern:Image_Objects) -> int:
-    count = 0
-    for line in image:
-        for pixel in line:
-            if (pixel == pattern.value).all():
-                count += 1
-
-    return count
-
-def find_water_max_line(image:np.ndarray) -> int:
-
-    max_line = 0
-    current_line = 0
-    max_pixels = 0
-    for line in image:
-        count_pixels = 0
-        for pixel in line:
-            if(pixel == Image_Objects.WATER.value).all():
-                count_pixels += 1
-        
-        if count_pixels > max_pixels:
-            max_line = current_line
-            max_pixels = count_pixels
-        current_line += 1
-
-    return max_line
-
-def map_lakes_coord(line:np.ndarray) -> list:
-    water_points = []
-
-    for i in range(len(line)):
-        if (line[i] == Image_Objects.WATER.value).all():
-            water_points.append(i)
-
-    return water_points
-
-def map_meteors_coords(image:np.ndarray) -> list:
-    meteors_map = []
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
-            if (image[i][j] == Image_Objects.METEOR.value).all():
-                meteors_map.append(j)
-    return meteors_map
+            if (image[i][j] == Image_Objects.STAR.value).all():
+                star_coords.append((i,j))
+            elif (image[i][j] == Image_Objects.METEOR.value).all():
+                meteor_coords.append((i,j))
+            elif (image[i][j] == Image_Objects.WATER.value).all():
+                lake_coords.append((i,j))
 
-def counting_stars(image:np.ndarray) -> int:
-    pattern = Image_Objects.STAR
-    return counting_pixel(image,pattern)
+    return star_coords,meteor_coords,lake_coords
 
-def counting_meteors(image:np.ndarray) -> int:
-    pattern = Image_Objects.METEOR
-    return counting_pixel(image,pattern)
+def find_water_line(lakes:list) -> int:
+    max_value = max(lakes)[0] + 1
+    counter = np.zeros(max_value)
+    for points in lakes:
+        counter[points[0]] += 1
 
-def counting_perpendicular_meteors(image:np.ndarray) -> int:
-    water_max_line = find_water_max_line(image)
-    lakes_coords = map_lakes_coord(image[water_max_line])
-    meteor_maps = map_meteors_coords(image)
+    counter_list = counter.tolist()
+    max_counter = max(counter_list)
+    return counter_list.index(max_counter)
 
+def clean_lakes_coord(lakes:list,line:int) -> list:
+    clean_lakes = []
+    for point in lakes:
+        if point[0] == line:
+            clean_lakes.append(point[1])
+    
+    return clean_lakes
+    
+
+def counting_perpendicular_meteors(meteors:list,lakes:list) -> int:
+    line_of_lakes = find_water_line(lakes)
+    lake_points = clean_lakes_coord(lakes,line_of_lakes)
     count = 0
-    for meteor in meteor_maps:
-        for lake_point in lakes_coords:
-            if meteor == lake_point:
+    for meteor in meteors:
+        for point in lake_points:
+            if meteor[1] == point:
                 count += 1
-
+                break
     return count
     
 
 def main(name_image:str) -> None:
     
     image = cv2.imread(name_image,cv2.IMREAD_COLOR)
-    num_stars = counting_stars(image)
-    num_meteors = counting_meteors(image)
-    num_perpendicular_meteors = counting_perpendicular_meteors(image)
-    
+    stars,meteors,lakes = map_objects_coord(image)
+    num_stars = len(stars)
+    num_meteors = len(meteors)
+    num_perpendicular_meteors = counting_perpendicular_meteors(meteors,lakes)
+
     print(f"Numero de estrelas: {num_stars} \n Numero de meteoros: {num_meteors} \n Numero de meteoros penpendiculares a agua: {num_perpendicular_meteors}")
 
 main(sys.argv[1])
